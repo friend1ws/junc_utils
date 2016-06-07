89c75daef0cd6b2cf7f8d83c3d7240ab115740c2
friend1ws <friend1ws@gmail.com>
2016-06-08 06:42:18 +0900

    first commit. add merge_control functions

---
+++ lib/junc_utils/run.py
@@ -0,0 +1,85 @@
+#! /usr/bin/env python
+
+import os, subprocess
+
+
+def merge_control_main(args):
+
+    input_file_list = args.junc_list
+    output_file = args.output_path
+    read_num_thres = args.read_num_thres
+    overhang_thres = args.overhang_thres
+    remove_annotated = False if args.keep_annotated else True
+
+    # make directory for output if necessary
+    if os.path.dirname(output_file) != "" and not os.path.exists(os.path.dirname(output_file)):
+        os.makedirs(os.path.dirname(output_file))
+
+    hin = open(input_file_list, 'r')
+    hout = open(output_file + ".unsroted", 'w')
+
+    with open(input_file_list, 'r') as hin:
+        for line in hin:
+
+            junction_file = line.rstrip('\n')
+            with open(junction_file, 'r') as hin2:
+                for line2 in hin2:
+
+                    F = line2.rstrip('\n').split('\t')
+                    if remove_annotated == True and F[5] != "0": continue
+                    if int(F[6]) < read_num_thres: continue 
+                    if int(F[8]) < overhang_thres: continue 
+         
+                    # convert to map-splice2 coordinate
+                    # F[1] = str(int(F[1]) - 1)
+                    # F[2] = str(int(F[2]) + 1)
+         
+                    print >> hout, '\t'.join(F)
+                
+
+    hout = open(output_file + ".sorted", 'w')
+    s_ret = subprocess.call(["sort", "-k1,1", "-k2,2n", "-k3,3n", output_file + ".unsroted"], stdout = hout)
+    hout.close()
+
+    if s_ret != 0:
+        print >> sys.stderr, "Error in sorting merged junction file"
+        sys.exit(1)
+
+
+    hout = open(output_file + ".merged", 'w')
+    with open(output_file + ".sorted", 'r') as hin:
+        temp_key = ""
+        temp_read_num = 0
+        for line in hin:
+            F = line.rstrip('\n').split('\t')
+            key = F[0] + '\t' + F[1] + '\t' + F[2]
+            read_num = int(F[6])
+            if key != temp_key:
+                if temp_key != "":
+                    print >> hout, temp_key + '\t' + str(read_num)
+                temp_key = key
+                temp_read_num = 0
+            else:
+                if read_num > temp_read_num:
+                    temp_read_num = read_num
+
+        print >> hout, temp_key + '\t' + str(read_num)
+
+
+    hout = open(output_file, 'w')
+    s_ret = subprocess.call(["bgzip", "-f", "-c", output_file + ".merged"], stdout = hout)
+    hout.close()
+
+    if s_ret != 0:
+        print >> sys.stderr, "Error in compression merged junction file"
+        sys.exit(1)
+
+
+    s_ret = subprocess.call(["tabix", "-p", "bed", output_file])
+    if s_ret != 0:
+        print >> sys.stderr, "Error in indexing merged junction file"
+        sys.exit(1)
+
+    subprocess.call(["rm", "-f", output_file + ".unsroted"])
+    subprocess.call(["rm", "-f", output_file + ".sorted"])
+    subprocess.call(["rm", "-f", output_file + ".merged"])
---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='junc_utils',
+      version='0.1.0beta',
+      description='Python programs for analyzing splicing junctions',
+      author='Yuichi Shiraishi',
+      author_email='friend1ws@gamil.com',
+      url='https://github.com/friend1ws/sv_utils',
+      package_dir = {'': 'lib'},
+      packages=['junc_utils'],
+      scripts=['junc_utils'],
+      license='GPL-3'
+     )
+
