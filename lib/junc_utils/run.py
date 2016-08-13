b27fd0dea76b6dfd032b1d5cf13c76868e477917
friend1ws <friend1ws@gmail.com>
2016-08-13 15:58:33 +0900

    modify output file names for associate command

--- lib/junc_utils/run.py
+++ lib/junc_utils/run.py
@@ -19,17 +19,17 @@ def annotate_main(args):
 def associate_main(args):
 
     mutation_file = args.mutation_file
-    output_prefix = args.output_prefix
-    is_anno = True if args.f == "anno" else False
+    output_file = args.output_file
+    is_anno = True if args.mutation_format == "anno" else False
     # control_file = args.ctrl
     is_sv = True if args.sv else False
     is_debug = True if args.debug else False 
     reference_genome = args.reference_genome
 
 
-    output_prefix_dir = os.path.dirname(output_prefix)
-    if output_prefix_dir != "" and not os.path.exists(output_prefix_dir):
-       os.makedirs(output_prefix_dir)
+    output_dir = os.path.dirname(output_file)
+    if output_dir != "" and not os.path.exists(output_file):
+       os.makedirs(output_dir)
 
     ##########
     # processing mutation file
@@ -38,47 +38,47 @@ def associate_main(args):
 
         # convert mutation data to vcf (if --anno is on)
         if is_anno == True:
-            utils.convert_anno2vcf(mutation_file, output_prefix + ".mutran_tmp.unsorted.vcf", reference_genome)
+            utils.convert_anno2vcf(mutation_file, output_file + ".mutran_tmp.unsorted.vcf", reference_genome)
         else:
-            utils.remove_vcf_header(mutation_file, output_prefix + ".mutran_tmp.unsorted.vcf")
+            utils.remove_vcf_header(mutation_file, output_file + ".mutran_tmp.unsorted.vcf")
 
-        hout = open(output_prefix + ".mutran_tmp.vcf", 'w')
-        s_ret = subprocess.call(["sort", "-k1,1", "-k2,2n", output_prefix + ".mutran_tmp.unsorted.vcf"], stdout = hout)
+        hout = open(output_file + ".mutran_tmp.vcf", 'w')
+        s_ret = subprocess.call(["sort", "-k1,1", "-k2,2n", output_file + ".mutran_tmp.unsorted.vcf"], stdout = hout)
         hout.close()
 
         if s_ret != 0:
             print >> sys.stderr, "Error in sorting vcf file"
             sys.exit(1)
 
-        s_ret = subprocess.call(["bgzip", "-f", output_prefix + ".mutran_tmp.vcf"])
+        s_ret = subprocess.call(["bgzip", "-f", output_file + ".mutran_tmp.vcf"])
         if s_ret != 0:
             print >> sys.stderr, "Error in bgzip compression"
             sys.exit(1)
 
-        s_ret = subprocess.call(["tabix", "-p", "vcf", output_prefix + ".mutran_tmp.vcf.gz"])
+        s_ret = subprocess.call(["tabix", "-p", "vcf", output_file + ".mutran_tmp.vcf.gz"])
         if s_ret != 0:
             print >> sys.stderr, "Error in tabix indexing"
             sys.exit(1)
 
     else:
         
-        utils.convert_genosv2bed(mutation_file, output_prefix + ".mutran_tmp.unsorted.bedpe")
+        utils.convert_genosv2bed(mutation_file, output_file + ".mutran_tmp.unsorted.bedpe")
 
-        hout = open(output_prefix + ".mutran_tmp.bedpe", 'w') 
-        s_ret = subprocess.call(["sort", "-k1,1", "-k2,2n", "-k3,3n", "-k4,4", "-k5,5n", "-k6,6n", output_prefix + ".mutran_tmp.unsorted.bedpe"], stdout = hout)
+        hout = open(output_file + ".mutran_tmp.bedpe", 'w') 
+        s_ret = subprocess.call(["sort", "-k1,1", "-k2,2n", "-k3,3n", "-k4,4", "-k5,5n", "-k6,6n", output_file + ".mutran_tmp.unsorted.bedpe"], stdout = hout)
         hout.close()
         
         if s_ret != 0:
             print >> sys.stderr, "Error in sorting bedpe file"
             sys.exit(1)
     
-        s_ret = subprocess.call(["bgzip", "-f", output_prefix + ".mutran_tmp.bedpe"])
+        s_ret = subprocess.call(["bgzip", "-f", output_file + ".mutran_tmp.bedpe"])
         if s_ret != 0:
             print >> sys.stderr, "Error in bgzip compression"
             sys.exit(1)
     
         
-        s_ret = subprocess.call(["tabix", "-p", "bed", output_prefix + ".mutran_tmp.bedpe.gz"])
+        s_ret = subprocess.call(["tabix", "-p", "bed", output_file + ".mutran_tmp.bedpe.gz"])
         if s_ret != 0:
             print >> sys.stderr, "Error in tabix indexing"
             sys.exit(1)
@@ -89,28 +89,28 @@ def associate_main(args):
     # associate mutation and junction
     if not is_sv:
         associate.get_snv_junction(args.annotated_junction_file,
-                                   output_prefix + ".splicing_mutation.txt",
-                                   output_prefix + ".mutran_tmp.vcf.gz",
+                                   output_file,
+                                   output_file + ".mutran_tmp.vcf.gz",
                                    args.annotation_dir)
     else:
         associate.get_sv_junction(args.annotated_junction_file,
-                                  output_prefix + ".splicing_sv.txt",
+                                  output_file,
                                   output_prefix + ".mutran_tmp.bedpe.gz",               
                                   args.annotation_dir)
     ##########
 
     if is_debug != True:
         if not is_sv:
-            subprocess.call(["rm", "-rf", output_prefix + ".mutran_tmp.unsorted.vcf"])
-            subprocess.call(["rm", "-rf", output_prefix + ".mutran_tmp.vcf.gz"])
-            subprocess.call(["rm", "-rf", output_prefix + ".mutran_tmp.vcf.gz.tbi"])
+            subprocess.call(["rm", "-rf", output_file + ".mutran_tmp.unsorted.vcf"])
+            subprocess.call(["rm", "-rf", output_file + ".mutran_tmp.vcf.gz"])
+            subprocess.call(["rm", "-rf", output_file + ".mutran_tmp.vcf.gz.tbi"])
         else:
-            subprocess.call(["rm", "-rf", output_prefix + ".mutran_tmp.unsorted.bedpe"])
-            subprocess.call(["rm", "-rf", output_prefix + ".mutran_tmp.bedpe.gz"])
-            subprocess.call(["rm", "-rf", output_prefix + ".mutran_tmp.bedpe.gz.tbi"])
+            subprocess.call(["rm", "-rf", output_file + ".mutran_tmp.unsorted.bedpe"])
+            subprocess.call(["rm", "-rf", output_file + ".mutran_tmp.bedpe.gz"])
+            subprocess.call(["rm", "-rf", output_file + ".mutran_tmp.bedpe.gz.tbi"])
 
-        subprocess.call(["rm", "-rf", output_prefix + ".mutran_tmp.junction.txt"])
-        subprocess.call(["rm", "-rf", output_prefix + ".mutran_tmp.junction.annot.txt"])
+        subprocess.call(["rm", "-rf", output_file + ".mutran_tmp.junction.txt"])
+        subprocess.call(["rm", "-rf", output_file + ".mutran_tmp.junction.annot.txt"])
 
 
 
